3d1f9d3349449cff40be35fc755b221a10fa858f
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2023-11-16 09:43:08 +0530

    Update requesthandlers.js

--- Bookmyshow/requesthandlers.js
+++ Bookmyshow/requesthandlers.js
@@ -2,11 +2,28 @@ import movieSchema from "./schema/movie.schema.js";
 
 
 
-export async function uploadData(req,res){
+   export async function uploadFilm(req,res){
     try {
-        
+        let { title } = req.body;
+        let filename = req.file.filename;
+        const newFilm = new Film({ title, filename });
+        await newFilm.save();
+        res.json({ success: true });
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({ success: false });
+    }
+
+}
+
+
+export async function getMovies(req,res){
+    try {
+        const movies = await Film.find({}, { _id: 0, __v: 0 });
+        res.json(movies);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false });
     }
 
 }
